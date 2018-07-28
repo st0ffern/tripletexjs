9daf5df69ec667171c9140da8b7a21d448e276ae
Simen A. W. Olsen <cobraz@cobraz.no>
2018-07-28 23:02:06 +0200

    revert back

--- swagger-gen.sh
+++ swagger-gen.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-swagger-codegen generate -i https://tripletex.no/v2/swagger.json -l typescript-fetch -o ./ --config ./config.json --template-dir ./templates/
+swagger-codegen generate -i https://tripletex.no/v2/swagger.json -l typescript-node -o ./ --config ./config.json --template-dir ./templates/
