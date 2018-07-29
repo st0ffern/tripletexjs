7d2b766d5c7231f5cb560aa9ae0f0fec43b0d938
Simen A. W. Olsen <cobraz@cobraz.no>
2018-07-29 20:42:18 +0200

    remove dist (?) + fix travis

--- dist/session.d.ts
+++
@@ -1,3 +0,0 @@
-export declare class Session {
-    static generateToken(consumerToken: string, employeeToken: string, expirationDate: string): Promise<import("../../../../Users/cobraz/Repos/tripletexjs/api").ResponseWrapperSessionToken>;
-}
